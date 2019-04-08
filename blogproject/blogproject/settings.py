c8db6cc0358d528d6271a18c53cab8ff164ca72b
XK <15882022301@163.com>
2019-04-08 14:04:56 +0800

    2

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -124,4 +124,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
